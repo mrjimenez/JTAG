4198695f3b32e3845f6979034cdcde77fa9d797b
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2017-07-19 22:30:15 -0300

    Fix compiling the example for esp01

--- examples/JTAGTest/JTAGTest.ino
+++ examples/JTAGTest/JTAGTest.ino
@@ -6,10 +6,14 @@
 // The maximum size of the arduino receive buffer is 63 bytes by default.
 // This number can be changed if you change the compiler flags in the
 // file hardware/arduino/avr/platform.local.txt (-O2 makes it faster):
-// 
+//
 // compiler.c.extra_flags=-DSERIAL_BUFFER_SIZE=256 -DSERIAL_RX_BUFFER_SIZE=256 -O2
 // compiler.cpp.extra_flags=-DSERIAL_BUFFER_SIZE=256 -DSERIAL_RX_BUFFER_SIZE=256 -O2
 //
+
+#ifndef SERIAL_RX_BUFFER_SIZE
+#define SERIAL_RX_BUFFER_SIZE 64
+#endif /* SERIAL_RX_BUFFER_SIZE */
 
 void setup()
 {
@@ -30,4 +34,3 @@ void loop()
 	// Currently, the PlayXSVFJTAGArduino object consumes 1042 bytes of RAM
 	p.play();
 }
-
